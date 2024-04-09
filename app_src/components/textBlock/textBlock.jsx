e6be8217f7e7e75faaab50c2dec4c616d183e28b
Skytowz <lucashottin359@gmail.com>
2024-04-09 14:48:27 +0200

    Premier commit de test

--- app_src/components/textBlock/textBlock.jsx
+++ app_src/components/textBlock/textBlock.jsx
@@ -1,66 +1,72 @@
-import './textBlock.scss';
+import "./textBlock.scss";
 
-import React from 'react';
-import {FiArrowRightCircle, FiTarget} from "react-icons/fi";
+import React from "react";
+import { FiArrowRightCircle, FiTarget } from "react-icons/fi";
 
-import config from '../../config';
-import {locale, setActiveLayerText, resizeTextArea, scrollToLine} from '../../utils';
-import {useContext} from '../../context';
-
+import config from "../../config";
+import { locale, setActiveLayerText, resizeTextArea, scrollToLine } from "../../utils";
+import { useContext } from "../../context";
 
 const TextBlock = React.memo(function TextBlock() {
-    const context = useContext();
-    const [focused, setFocused] = React.useState(false);
-    React.useEffect(resizeTextArea);
-    React.useEffect(() => {
-        scrollToLine(context.state.currentLineIndex, 1000);
-    }, []);
-    return (
-        <React.Fragment>
-            <div className="text-lines">
-                {context.state.lines.map(line => (
-                    <div key={line.rawIndex} className={'text-line' + (line.ignore ? ' m-empty' : '') + ((context.state.currentLineIndex === line.rawIndex) ? ' m-current' : '')}>
-                        <div className="text-line-num">
-                            {line.ignore ? ' ' : line.index}
-                        </div>
-                        <div className="text-line-select" title={line.ignore ? '' : locale.selectLine}>
-                            {line.ignore ? ' ' : <FiTarget size={14} onClick={() => context.dispatch({type: 'setCurrentLineIndex', index: line.rawIndex})} />}
-                        </div>
-                        <div className="text-line-text">
-                            {line.ignorePrefix ? (
-                                <React.Fragment>
-                                    <span className="text-line-ignore-prefix">{line.ignorePrefix}</span>
-                                    <span>{line.rawText.replace(line.ignorePrefix, '')}</span>
-                                </React.Fragment>
-                            ) : line.stylePrefix ? (
-                                <React.Fragment>
-                                    <span className="text-line-style-prefix" style={{background: line.style?.prefixColor || config.defaultPrefixColor}}>{line.stylePrefix}</span>
-                                    <span>{line.rawText.replace(line.stylePrefix, '')}</span>
-                                </React.Fragment>
-                            ) : (
-                                <span>{line.rawText || ' '}</span>
-                            )}
-                        </div>
-                        <div className="text-line-insert" title={line.ignore ? '' : locale.insertText}>
-                            {line.ignore ? ' ' : <FiArrowRightCircle size={14} onClick={() => setActiveLayerText(line.text)} />}
-                        </div>
-                    </div>
-                ))}
+  const context = useContext();
+  const [focused, setFocused] = React.useState(false);
+  React.useEffect(resizeTextArea);
+  React.useEffect(() => {
+    scrollToLine(context.state.currentLineIndex, 1000);
+  }, []);
+  return (
+    <React.Fragment>
+      <div className="text-lines">
+        {context.state.lines.map((line) => (
+          <div key={line.rawIndex} className={"text-line" + (line.ignore ? " m-empty" : "") + (context.state.currentLineIndex === line.rawIndex ? " m-current" : "")}>
+            <div className="text-line-num">{line.ignore ? " " : line.index}</div>
+            <div className="text-line-select" title={line.ignore ? "" : locale.selectLine}>
+              {line.ignore ? " " : <FiTarget size={14} onClick={() => context.dispatch({ type: "setCurrentLineIndex", index: line.rawIndex })} />}
             </div>
-            <textarea 
-                className="text-area" 
-                value={context.state.text} 
-                onChange={e => context.dispatch({type: 'setText', text: e.target.value})} 
-                onFocus={() => setFocused(true)}
-                onBlur={() => setFocused(false)}
-            />
-            {!context.state.lines.length && !focused && (
-                <div className="text-message">
-                    <div>{locale.pasteTextHint}</div>
-                </div>
-            )}
-        </React.Fragment>
-    );
+            <div className="text-line-text">
+              {line.ignorePrefix ? (
+                <React.Fragment>
+                  <span className="text-line-ignore-prefix">{line.ignorePrefix}</span>
+                  <span>{line.rawText.replace(line.ignorePrefix, "")}</span>
+                </React.Fragment>
+              ) : line.stylePrefix ? (
+                <React.Fragment>
+                  <span className="text-line-style-prefix" style={{ background: line.style?.prefixColor || config.defaultPrefixColor }}>
+                    {line.stylePrefix}
+                  </span>
+                  <span>{line.rawText.replace(line.stylePrefix, "")}</span>
+                </React.Fragment>
+              ) : (
+                <span>{line.rawText || " "}</span>
+              )}
+            </div>
+            <div className="text-line-insert" title={line.ignore ? "" : locale.insertText}>
+              {line.ignore ? " " : <FiArrowRightCircle size={14} onClick={() => setActiveLayerText(line.text)} />}
+            </div>
+          </div>
+        ))}
+      </div>
+      <textarea className="text-area" value={context.state.text} onChange={(e) => context.dispatch({ type: "setText", text: e.target.value })} onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} />
+      {!context.state.lines.length && !focused && (
+        <div className="text-message">
+          <div>{locale.pasteTextHint}</div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 });
+
+const classNameLine = (line, context) => {
+  let style = "text-line";
+  if (line.ignore) {
+    style += " m-empty";
+  }
+  if (context.state.currentLineIndex === line.rawIndex) {
+    style += " m-current";
+  }
+  if (line.rawText.match("Page [0-9]+")) {
+    style += " m-page";
+  }
+};
 
 export default TextBlock;
