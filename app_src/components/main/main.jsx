44007c8b0e3bdf7641b55098f17abe6f778ff6fa
SeanR-ScanR <scanr.seanr@gmail.com>
2024-06-19 00:21:21 +0200

    Fixed #7

--- app_src/components/main/main.jsx
+++ app_src/components/main/main.jsx
@@ -1,92 +1,90 @@
-import './main.scss';
+import "./main.scss";
 
-import React from 'react';
-import {readStorage, writeToStorage, resizeTextArea} from '../../utils';
-import Modal from '../modal/modal';
-import TextBlock from '../textBlock/textBlock';
-import PreviewBlock from '../previewBlock/previewBlock';
-import StylesBlock from '../stylesBlock/stylesBlock';
-import AppFooter from '../footer/footer';
-
+import React from "react";
+import { readStorage, writeToStorage, resizeTextArea } from "../../utils";
+import Modal from "../modal/modal";
+import TextBlock from "../textBlock/textBlock";
+import PreviewBlock from "../previewBlock/previewBlock";
+import StylesBlock from "../stylesBlock/stylesBlock";
+import AppFooter from "../footer/footer";
 
 const topHeight = 130;
-const minMiddleHeight = 160;
-const minBottomHeight = 260;
+const minMiddleHeight = 100;
+const minBottomHeight = 70;
 
 const ResizeableCont = React.memo(function ResizeableCont() {
-    const appBlock = React.useRef();
-    const bottomBlock = React.useRef();
+  const appBlock = React.useRef();
+  const bottomBlock = React.useRef();
 
-    let dragging = false;
-    let resizeStartY = 0;
-    let resizeStartH = 0;
-    let bottomHeight = 0;
-    let appHeight = 0;
+  let dragging = false;
+  let resizeStartY = 0;
+  let resizeStartH = 0;
+  let bottomHeight = 0;
+  let appHeight = 0;
 
-    const startBottomResize = e => {
-        resizeStartH = bottomBlock.current.offsetHeight;
-        resizeStartY = e.pageY;
-        dragging = true;
-    };
+  const startBottomResize = (e) => {
+    resizeStartH = bottomBlock.current.offsetHeight;
+    resizeStartY = e.pageY;
+    dragging = true;
+  };
 
-    const stopBottomResize = () => {
-        if (dragging) {
-            writeToStorage({bottomHeight});
-            dragging = false;
-        }
-        
-    };
+  const stopBottomResize = () => {
+    if (dragging) {
+      writeToStorage({ bottomHeight });
+      dragging = false;
+    }
+  };
 
-    const moveBottomResize = e => {
-        if (dragging) {
-            e.preventDefault();
-            const dy = e.pageY - resizeStartY;
-            const newHeight = resizeStartH - dy;
-            setBottomSize(newHeight);
-        }
-    };
+  const moveBottomResize = (e) => {
+    if (dragging) {
+      e.preventDefault();
+      const dy = e.pageY - resizeStartY;
+      const newHeight = resizeStartH - dy;
+      setBottomSize(newHeight);
+    }
+  };
 
-    const setBottomSize = height => {
-        const maxBottomHeight = appHeight - topHeight - minMiddleHeight;
-        bottomHeight = height || readStorage('bottomHeight') || minBottomHeight;
-        if (height < minBottomHeight) bottomHeight = minBottomHeight;
-        if (height > maxBottomHeight) bottomHeight = maxBottomHeight;
-        bottomBlock.current.style.height = bottomHeight + 'px';
-        resizeTextArea();
-    };
+  const setBottomSize = (height) => {
+    const maxBottomHeight = appHeight - (appHeight > 450 ? topHeight : 0) - minMiddleHeight;
+    bottomHeight = height || readStorage("bottomHeight") || minBottomHeight;
+    if (height < minBottomHeight) bottomHeight = minBottomHeight;
+    if (height > maxBottomHeight) bottomHeight = maxBottomHeight;
+    bottomBlock.current.style.height = bottomHeight + "px";
+    resizeTextArea();
+  };
 
-    const setAppSize = () => {
-        appHeight = document.documentElement.clientHeight;
-        appBlock.current.style.height = appHeight + 'px';
-        setBottomSize();
-    };
+  const setAppSize = () => {
+    appHeight = document.documentElement.clientHeight;
+    appBlock.current.style.height = appHeight + "px";
+    setBottomSize();
+  };
 
-    React.useEffect(() => {
-        window.addEventListener('resize', setAppSize);
-        setAppSize();
-    }, []);
+  React.useEffect(() => {
+    window.addEventListener("resize", setAppSize);
+    setAppSize();
+  }, []);
 
-    return (
-        <div className="app-body" ref={appBlock} onMouseMove={moveBottomResize} onMouseLeave={stopBottomResize} onMouseUp={stopBottomResize}>
-            <Modal />
-            <div className="top-block preview-block" style={{height: topHeight}}>
-                <PreviewBlock />
-            </div>
-            <div className="top-divider hostBgdDark"></div>
-            <div className="middle-block text-block">
-                <TextBlock />
-            </div>
-            <div className="middle-divider hostBgdDark" onMouseDown={startBottomResize}>
-                <div className="hostBgdLight"></div>
-            </div>
-            <div className="bottom-block styles-block" ref={bottomBlock}>
-                <StylesBlock />
-            </div>
-            <div className="footer-block hostBrdTopContrast">
-                <AppFooter />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app-body" ref={appBlock} onMouseMove={moveBottomResize} onMouseLeave={stopBottomResize} onMouseUp={stopBottomResize}>
+      <Modal />
+      <div className="top-block preview-block" style={{ height: topHeight }}>
+        <PreviewBlock />
+      </div>
+      <div className="top-divider hostBgdDark"></div>
+      <div className="middle-block text-block">
+        <TextBlock />
+      </div>
+      <div className="middle-divider hostBgdDark" onMouseDown={startBottomResize}>
+        <div className="hostBgdLight"></div>
+      </div>
+      <div className="bottom-block styles-block" ref={bottomBlock}>
+        <StylesBlock />
+      </div>
+      <div className="footer-block hostBrdTopContrast">
+        <AppFooter />
+      </div>
+    </div>
+  );
 });
 
 export default ResizeableCont;
--- app_src/components/stylesBlock/stylesBlock.jsx
+++ app_src/components/stylesBlock/stylesBlock.jsx
@@ -1,169 +1,147 @@
-import './stylesBlock.scss';
+import "./stylesBlock.scss";
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import {ReactSortable} from "react-sortablejs";
-import {FiArrowRightCircle, FiPlus, FiFolderPlus, FiChevronDown, FiChevronUp} from "react-icons/fi";
-import {MdEdit, MdLock} from "react-icons/md";
+import React from "react";
+import PropTypes from "prop-types";
+import { ReactSortable } from "react-sortablejs";
+import { FiArrowRightCircle, FiPlus, FiFolderPlus, FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { MdEdit, MdLock } from "react-icons/md";
 
-import config from '../../config';
-import {locale, getActiveLayerText, setActiveLayerText, rgbToHex, getStyleObject} from '../../utils';
-import {useContext} from '../../context';
-
+import config from "../../config";
+import { locale, getActiveLayerText, setActiveLayerText, rgbToHex, getStyleObject } from "../../utils";
+import { useContext } from "../../context";
 
 const StylesBlock = React.memo(function StylesBlock() {
-    const context = useContext();
-    const unsortedStyles = context.state.styles.filter(s => !s.folder);
-    return (
-        <React.Fragment>
-            <div className="folders-list">
-                {(context.state.folders.length || context.state.styles.length) ? (
-                    <React.Fragment>
-                        {(unsortedStyles.length > 0) && (
-                            <FolderItem 
-                                data={{name: locale.noFolderTitle}}
-                            />
-                        )}
-                        <ReactSortable className="folders-sortable" list={context.state.folders} setList={data => context.dispatch({type: 'setFolders', data})}>
-                            {context.state.folders.map(folder => (
-                                <FolderItem 
-                                    key={folder.id} 
-                                    data={folder}
-                                />
-                            ))}
-                        </ReactSortable>
-                    </React.Fragment>
-                ) : (
-                    <div className="styles-empty">
-                        <span>{locale.addStylesHint}</span>
-                    </div>
-                )}
-            </div>
-            <div className="style-add hostBrdTopContrast">
-                <button className="topcoat-button--large" onClick={() => context.dispatch({type: 'setModal', modal: 'editFolder', data: {create: true}})}>
-                    <FiFolderPlus size={18} /> {locale.addFolder}
-                </button>
-                <button className="topcoat-button--large" onClick={() => context.dispatch({type: 'setModal', modal: 'editStyle', data: {create: true}})}>
-                    <FiPlus size={18} /> {locale.addStyle}
-                </button>
-            </div>
-        </React.Fragment>
-    );
+  const context = useContext();
+  const unsortedStyles = context.state.styles.filter((s) => !s.folder);
+  return (
+    <React.Fragment>
+      <div className="folders-list">
+        {context.state.folders.length || context.state.styles.length ? (
+          <React.Fragment>
+            {unsortedStyles.length > 0 && <FolderItem data={{ name: locale.noFolderTitle }} />}
+            <ReactSortable className="folders-sortable" list={context.state.folders} setList={(data) => context.dispatch({ type: "setFolders", data })}>
+              {context.state.folders.map((folder) => (
+                <FolderItem key={folder.id} data={folder} />
+              ))}
+            </ReactSortable>
+          </React.Fragment>
+        ) : (
+          <div className="styles-empty">
+            <span>{locale.addStylesHint}</span>
+          </div>
+        )}
+      </div>
+      <div className="style-add hostBrdTopContrast style-btn-list">
+        <button className="topcoat-button--large" onClick={() => context.dispatch({ type: "setModal", modal: "editFolder", data: { create: true } })}>
+          <FiFolderPlus size={18} /> {locale.addFolder}
+        </button>
+        <button className="topcoat-button--large" onClick={() => context.dispatch({ type: "setModal", modal: "editStyle", data: { create: true } })}>
+          <FiPlus size={18} /> {locale.addStyle}
+        </button>
+      </div>
+    </React.Fragment>
+  );
 });
-
 
 const FolderItem = React.memo(function FolderItem(props) {
-    const context = useContext();
-    const openFolder = e => {
-        e.stopPropagation();
-        context.dispatch({type: 'setModal', modal: 'editFolder', data: props.data})
-    };
-    const sortFolderStyles = folderStyles => {
-        let styles = props.data.id ? context.state.styles.filter(s => (s.folder !== props.data.id)) : context.state.styles.filter(s => !!s.folder);
-        styles = styles.concat(folderStyles);
-        context.dispatch({type: 'setStyles', data: styles});
-    };
-    const styles = props.data.id ? context.state.styles.filter(s => (s.folder === props.data.id)) : context.state.styles.filter(s => !s.folder);
-    const isOpen = props.data.id ? context.state.openFolders.includes(props.data.id) : context.state.openFolders.includes('unsorted');
-    const hasActive = context.state.currentStyleId ? !!styles.find(s => (s.id === context.state.currentStyleId)) : false;
-    return (
-        <div className={'folder-item hostBrdContrast' + (isOpen ? ' m-open' : '')}>
-            <div className="folder-header" onClick={() => context.dispatch({type: 'toggleFolder', id: props.data.id})}>
-                <div className="folder-marker">
-                    {isOpen ? <FiChevronUp size={18} /> : <FiChevronDown size={18} />}
-                </div>
-                <div className="folder-title">
-                    {hasActive ? <strong>{props.data.name}</strong> : <span>{props.data.name}</span>}
-                    <em className="folder-styles-count">({styles.length})</em>
-                </div>
-                <div className="folder-actions">
-                    {props.data.id ? (
-                        <button className="topcoat-icon-button--large--quiet" title={locale.editFolder} onClick={openFolder}>
-                            <MdEdit size={14} />
-                        </button>
-                    ) : (
-                        <MdLock size={18} className="folder-locked" />
-                    )}
-                </div>
-            </div>
-            {isOpen && (
-                <div className="folder-styles hostBrdTopContrast">
-                    {styles.length ? (
-                        <ReactSortable className="styles-list" list={styles} setList={sortFolderStyles}>
-                            {styles.map(style => (
-                                <StyleItem 
-                                    key={style.id} 
-                                    active={context.state.currentStyleId === style.id}
-                                    selectStyle={() => context.dispatch({type: 'setCurrentStyleId', id: style.id})}
-                                    openStyle={() => context.dispatch({type: 'setModal', modal: 'editStyle', data: style})}
-                                    style={style}
-                                />
-                            ))}
-                        </ReactSortable >
-                    ) : (
-                        <div className="folder-styles-empty">
-                            <span>{locale.noStylesInfolder}</span>
-                        </div>
-                    )}
-                </div>
-            )}
+  const context = useContext();
+  const openFolder = (e) => {
+    e.stopPropagation();
+    context.dispatch({ type: "setModal", modal: "editFolder", data: props.data });
+  };
+  const sortFolderStyles = (folderStyles) => {
+    let styles = props.data.id ? context.state.styles.filter((s) => s.folder !== props.data.id) : context.state.styles.filter((s) => !!s.folder);
+    styles = styles.concat(folderStyles);
+    context.dispatch({ type: "setStyles", data: styles });
+  };
+  const styles = props.data.id ? context.state.styles.filter((s) => s.folder === props.data.id) : context.state.styles.filter((s) => !s.folder);
+  const isOpen = props.data.id ? context.state.openFolders.includes(props.data.id) : context.state.openFolders.includes("unsorted");
+  const hasActive = context.state.currentStyleId ? !!styles.find((s) => s.id === context.state.currentStyleId) : false;
+  return (
+    <div className={"folder-item hostBrdContrast" + (isOpen ? " m-open" : "")}>
+      <div className="folder-header" onClick={() => context.dispatch({ type: "toggleFolder", id: props.data.id })}>
+        <div className="folder-marker">{isOpen ? <FiChevronUp size={18} /> : <FiChevronDown size={18} />}</div>
+        <div className="folder-title">
+          {hasActive ? <strong>{props.data.name}</strong> : <span>{props.data.name}</span>}
+          <em className="folder-styles-count">({styles.length})</em>
         </div>
-    );
+        <div className="folder-actions">
+          {props.data.id ? (
+            <button className="topcoat-icon-button--large--quiet" title={locale.editFolder} onClick={openFolder}>
+              <MdEdit size={14} />
+            </button>
+          ) : (
+            <MdLock size={18} className="folder-locked" />
+          )}
+        </div>
+      </div>
+      {isOpen && (
+        <div className="folder-styles hostBrdTopContrast">
+          {styles.length ? (
+            <ReactSortable className="styles-list" list={styles} setList={sortFolderStyles}>
+              {styles.map((style) => (
+                <StyleItem key={style.id} active={context.state.currentStyleId === style.id} selectStyle={() => context.dispatch({ type: "setCurrentStyleId", id: style.id })} openStyle={() => context.dispatch({ type: "setModal", modal: "editStyle", data: style })} style={style} />
+              ))}
+            </ReactSortable>
+          ) : (
+            <div className="folder-styles-empty">
+              <span>{locale.noStylesInfolder}</span>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 });
 FolderItem.propTypes = {
-    data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
 };
 
-
 const StyleItem = React.memo(function StyleItem(props) {
-    const textStyle = props.style.textProps.layerText.textStyleRange[0]?.textStyle || {};
-    const styleObject = getStyleObject(textStyle);
-    const openStyle = e => {
-        e.stopPropagation();
-        props.openStyle();
-    };
-    const insertStyle = e => {
-        e.stopPropagation();
-        if (e.ctrlKey) {
-            getActiveLayerText(data => {
-                textStyle.size = data.textProps.layerText.textStyleRange[0].textStyle.size;
-                setActiveLayerText('', props.style);
-            });
-        } else {
-            setActiveLayerText('', props.style);
-        }
-    };
-    return (
-        <div id={props.style.id} className={'style-item hostBgdLight' + (props.active ? ' m-current' : '')} onClick={props.selectStyle}>
-            <div className="style-marker">
-                <div className="style-color" style={{background: rgbToHex(textStyle.color)}} title={locale.styleTextColor + ': ' + rgbToHex(textStyle.color)}></div>
-                {!!props.style.prefixes.length && (
-                    <div className="style-prefix-color" title={locale.stylePrefixColor + ': ' + (props.style.prefixColor || config.defaultPrefixColor)}>
-                        <div style={{background: props.style.prefixColor || config.defaultPrefixColor}}></div>
-                    </div>
-                )}
-            </div>
-            <div className="style-name" style={styleObject}
-                dangerouslySetInnerHTML = {
-                    { __html: `<span style='font-family: "${styleObject.fontFamily || 'Tahoma'}"'>${props.style.name}</span>`} 
-                }
-            ></div>
-            <div className="style-actions">
-                <button className={'topcoat-icon-button--large--quiet' + (props.active ? ' m-cta' : '')} title={locale.editStyle} onClick={openStyle}>
-                    <MdEdit size={16} />
-                </button>
-                <button className={'topcoat-icon-button--large--quiet' + (props.active ? ' m-cta' : '')} title={locale.insertStyle} onClick={insertStyle}>
-                    <FiArrowRightCircle size={16} />
-                </button>
-            </div>
-        </div>
-    );
+  const textStyle = props.style.textProps.layerText.textStyleRange[0]?.textStyle || {};
+  const styleObject = getStyleObject(textStyle);
+  const openStyle = (e) => {
+    e.stopPropagation();
+    props.openStyle();
+  };
+  const insertStyle = (e) => {
+    e.stopPropagation();
+    if (e.ctrlKey) {
+      getActiveLayerText((data) => {
+        textStyle.size = data.textProps.layerText.textStyleRange[0].textStyle.size;
+        setActiveLayerText("", props.style);
+      });
+    } else {
+      setActiveLayerText("", props.style);
+    }
+  };
+  return (
+    <div id={props.style.id} className={"style-item hostBgdLight" + (props.active ? " m-current" : "")} onClick={props.selectStyle}>
+      <div className="style-marker">
+        <div className="style-color" style={{ background: rgbToHex(textStyle.color) }} title={locale.styleTextColor + ": " + rgbToHex(textStyle.color)}></div>
+        {!!props.style.prefixes.length && (
+          <div className="style-prefix-color" title={locale.stylePrefixColor + ": " + (props.style.prefixColor || config.defaultPrefixColor)}>
+            <div style={{ background: props.style.prefixColor || config.defaultPrefixColor }}></div>
+          </div>
+        )}
+      </div>
+      <div className="style-name" style={styleObject} dangerouslySetInnerHTML={{ __html: `<span style='font-family: "${styleObject.fontFamily || "Tahoma"}"'>${props.style.name}</span>` }}></div>
+      <div className="style-actions">
+        <button className={"topcoat-icon-button--large--quiet" + (props.active ? " m-cta" : "")} title={locale.editStyle} onClick={openStyle}>
+          <MdEdit size={16} />
+        </button>
+        <button className={"topcoat-icon-button--large--quiet" + (props.active ? " m-cta" : "")} title={locale.insertStyle} onClick={insertStyle}>
+          <FiArrowRightCircle size={16} />
+        </button>
+      </div>
+    </div>
+  );
 });
 StyleItem.propTypes = {
-    selectStyle: PropTypes.func.isRequired,
-    openStyle: PropTypes.func.isRequired,
-    style: PropTypes.object.isRequired,
-    active: PropTypes.bool
+  selectStyle: PropTypes.func.isRequired,
+  openStyle: PropTypes.func.isRequired,
+  style: PropTypes.object.isRequired,
+  active: PropTypes.bool,
 };
 
 export default StylesBlock;
